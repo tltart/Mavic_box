91e44950701f36eee44f662bdae1debda32bd6b6
Artem <art.artem001@yandex.ru>
2020-08-03 12:59:16 +0400

    03.08.2020

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+byte D4 = 4;
+byte D5 = 5;
+byte D6 = 6;
+byte D7 = 7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
